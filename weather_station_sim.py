57ef0f3fe08f2981b77a54e3d7912e1977ef95bb
michal <mongolow0072005@gmail.com>
2026-05-07 17:39:48 +0200

    finished weather station sim

---
+++ weather_station_sim.py
@@ -0,0 +1,11 @@
+from secrets import SystemRandom
+from requests import post
+from time import sleep
+
+if __name__ == "__main__":
+    while True:
+        post("http://127.0.0.1:5001/api/weather/post",
+             json ={'temp': round(SystemRandom().uniform(25.5, 30), 2),
+                    'hum': round(SystemRandom().uniform(30.1, 40), 2),
+                    'press': round(SystemRandom().uniform(950.1, 1000), 2)})
+        sleep(10)
